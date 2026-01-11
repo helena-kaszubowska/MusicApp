2034ac41448186a30fbafb28075518bffdafb044
Helena Kaszubowska <helenakaszubowska@wp.pl>
2026-01-11 18:40:01 +0100

    Fix URL

--- music-app-frontend/src/config/api.js
+++ music-app-frontend/src/config/api.js
@@ -1,6 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  "https://bbz40djmz4.execute-api.eu-north-1.amazonaws.com/default/api";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5064/api";
 
 export const API_ENDPOINTS = {
   albums: {
